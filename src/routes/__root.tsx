da010faa8ab4511eabe121fdff16b90be244f915
marcusgriffin212009-dotcom <marcusgriffin212009@gmail.com>
2026-08-02 17:59:36 -0400

    chore: remove favicon (use empty data URI)

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -85,7 +85,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "data:," },
     ],
   }),
   shellComponent: RootShell,
